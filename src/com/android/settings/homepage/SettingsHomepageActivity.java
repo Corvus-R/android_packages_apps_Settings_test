450d5652c9849fa49f0bde941645c48d5e3b0c73
Trishiraj <gamerprince.exp@gmail.com>
2022-01-24 14:46:20 +0000

    Revert "SearchBar: Add avatar to search bar"
    This reverts commit 736b9b37c5991d09ec2ff25264198342755eea60.
    
    Change-Id: If708ab1845c6e2b051693131fe721a8cfd49d294

--- src/com/android/settings/homepage/SettingsHomepageActivity.java
+++ src/com/android/settings/homepage/SettingsHomepageActivity.java
@@ -22,15 +22,8 @@ import androidx.annotation.Nullable;
 import android.animation.LayoutTransition;
 import android.app.ActivityManager;
 import android.app.settings.SettingsEnums;
-import android.content.ComponentName;
-import android.content.Context;
 import android.content.res.ColorStateList;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.os.UserHandle;
-import android.os.UserManager;
 import android.os.SystemProperties;
 import android.os.Build;
 import android.util.FeatureFlagUtils;
@@ -52,8 +45,6 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.fragment.app.FragmentPagerAdapter;
 
-import com.android.internal.util.UserIcons;
-
 import com.android.settings.R;
 import com.android.settings.Utils;
 import com.android.settings.accounts.AvatarViewMixin;
@@ -63,10 +54,7 @@ import com.android.settings.homepage.contextualcards.ContextualCardsFragment;
 import com.android.settings.overlay.FeatureFactory;
 import com.android.settingslib.core.lifecycle.HideNonSystemOverlayMixin;
 
-import com.android.settingslib.drawable.CircleFramedDrawable;
-
 import java.util.ArrayList;
-
 /** Settings homepage activity */
 public class SettingsHomepageActivity extends FragmentActivity implements
         CategoryMixin.CategoryHandler {
@@ -105,10 +93,8 @@ public class SettingsHomepageActivity extends FragmentActivity implements
         mHomepageView = null;
     }
 
-    Context context;
-    ImageView avatarView, btnCorvusVersion;
+    ImageView btnCorvusVersion;
     TextView crvsVersion, crvsMaintainer, crvsDevice, crvsBuildDate, crvsBuildType;
-    UserManager mUserManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -121,26 +107,9 @@ public class SettingsHomepageActivity extends FragmentActivity implements
         initHomepageContainer();
         setupTabIcons();
 
-        Context context = getApplicationContext();
-
-        mUserManager = context.getSystemService(UserManager.class);
-
         final Toolbar toolbar = findViewById(R.id.search_action_bar);
         FeatureFactory.getFactory(this).getSearchFeatureProvider()
                 .initSearchToolbar(this /* activity */, toolbar, SettingsEnums.SETTINGS_HOMEPAGE);
-
-        avatarView = findViewById(R.id.account_avatar);
-        //final AvatarViewMixin avatarViewMixin = new AvatarViewMixin(this, avatarView);
-        avatarView.setImageDrawable(getCircularUserIcon(context));
-        avatarView.setVisibility(View.VISIBLE);
-        avatarView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_MAIN);
-                intent.setComponent(new ComponentName("com.android.settings","com.android.settings.Settings$UserSettingsActivity"));
-                startActivity(intent);
-            }
-        });
 
         getLifecycle().addObserver(new HideNonSystemOverlayMixin(this));
         mCategoryMixin = new CategoryMixin(this);
@@ -148,6 +117,12 @@ public class SettingsHomepageActivity extends FragmentActivity implements
 
         if (!getSystemService(ActivityManager.class).isLowRamDevice()) {
             // Only allow features on high ram devices.
+            final ImageView avatarView = findViewById(R.id.account_avatar);
+            if (AvatarViewMixin.isAvatarSupported(this)) {
+                avatarView.setVisibility(View.VISIBLE);
+                getLifecycle().addObserver(new AvatarViewMixin(this, avatarView));
+            }
+
             showSuggestionFragment();
 
             if (FeatureFlagUtils.isEnabled(this, FeatureFlags.CONTEXTUAL_HOME)) {
@@ -243,27 +218,6 @@ public class SettingsHomepageActivity extends FragmentActivity implements
         final int searchBarHeight = getResources().getDimensionPixelSize(R.dimen.search_bar_height);
         final int searchBarMargin = getResources().getDimensionPixelSize(R.dimen.search_bar_margin);
         return searchBarHeight + searchBarMargin * 2;
-    }
-
-    private Drawable getCircularUserIcon(Context context) {
-        Bitmap bitmapUserIcon = mUserManager.getUserIcon(UserHandle.myUserId());
-
-        if (bitmapUserIcon == null) {
-            // get default user icon.
-            final Drawable defaultUserIcon = UserIcons.getDefaultUserIcon(
-                    context.getResources(), UserHandle.myUserId(), false);
-            bitmapUserIcon = UserIcons.convertToBitmap(defaultUserIcon);
-        }
-        Drawable drawableUserIcon = new CircleFramedDrawable(bitmapUserIcon,
-                (int) context.getResources().getDimension(R.dimen.circle_avatar_size));
-
-        return drawableUserIcon;
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        avatarView.setImageDrawable(getCircularUserIcon(getApplicationContext()));
     }
 
     static class ViewPagerAdapter extends FragmentPagerAdapter {
